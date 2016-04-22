22273a9874f8dd153fa6cd87466532dbc3cf5474
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-21 22:35:18 -0700

    ip

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
+server.listen(config.port, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
