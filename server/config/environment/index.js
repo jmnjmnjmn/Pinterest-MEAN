01d5e70def44e1182872ac8cb18125b36ffad88f
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-21 22:24:31 -0700

    secret

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -26,7 +26,7 @@ var all = {
 
   // Secret for session, you will want to change this and make it an environment variable
   secrets: {
-    session: 'meanApp-secret'
+    session: process.env.sessionSecret
   },
 
   // List of user roles
